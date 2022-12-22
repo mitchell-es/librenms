36fe5703381af6cece96f1c6977444ae90d6936a
David Mitchell <mitchell@es.net>
2022-12-22 21:03:54 +0000

    Grab the OIDs we use individually instead of walking the whole table We only use about 20% of the rows so grabbing all of them is a waste of time.

--- includes/discovery/sensors/pre-cache/timos.inc.php
+++ includes/discovery/sensors/pre-cache/timos.inc.php
@@ -9,5 +9,20 @@
  * option) any later version.  Please see LICENSE.txt at the top level of
  * the source code distribution for details.
  */
-
-$pre_cache['timos_oids'] = snmpwalk_cache_multi_oid($device, 'tmnxDigitalDiagMonitorEntry', [], 'TIMETRA-PORT-MIB', 'timos');
+#
+# These OIDS are all from tmnxDigitalDiagMonitorEntry. There are 40 more values in
+# that table which are not currently used so we gain some efficiency by not
+# walking the whole table. Also, this table tends to cause slowdowns for the snmp
+# process on the router being walked so these OIDs are configured to not use bulk
+# snmpbulkwalk in the timos OS definition file.
+$pre_cache['timos_oids'] = snmpwalk_cache_multi_oid($device, 'tmnxDDMRxOpticalPower', [], 'TIMETRA-PORT-MIB', 'timos');
+$pre_cache['timos_oids'] = snmpwalk_cache_multi_oid($device, 'tmnxDDMTxOutputPower', $pre_cache, 'TIMETRA-PORT-MIB', 'timos');
+$pre_cache['timos_oids'] = snmpwalk_cache_multi_oid($device, 'tmnxDDMExternallyCalibrated', $pre_cache, 'TIMETRA-PORT-MIB', 'timos');
+$pre_cache['timos_oids'] = snmpwalk_cache_multi_oid($device, 'tmnxDDMRxOpticalPowerLowAlarm', $pre_cache, 'TIMETRA-PORT-MIB', 'timos');
+$pre_cache['timos_oids'] = snmpwalk_cache_multi_oid($device, 'tmnxDDMRxOpticalPowerLowWarning', $pre_cache, 'TIMETRA-PORT-MIB', 'timos');
+$pre_cache['timos_oids'] = snmpwalk_cache_multi_oid($device, 'tmnxDDMRxOpticalPowerHiAlarm', $pre_cache, 'TIMETRA-PORT-MIB', 'timos');
+$pre_cache['timos_oids'] = snmpwalk_cache_multi_oid($device, 'tmnxDDMRxOpticalPowerHiWarning', $pre_cache, 'TIMETRA-PORT-MIB', 'timos');
+$pre_cache['timos_oids'] = snmpwalk_cache_multi_oid($device, 'tmnxDDMTxOutputPowerLowAlarm', $pre_cache, 'TIMETRA-PORT-MIB', 'timos');
+$pre_cache['timos_oids'] = snmpwalk_cache_multi_oid($device, 'tmnxDDMTxOutputPowerLowWarning', $pre_cache, 'TIMETRA-PORT-MIB', 'timos');
+$pre_cache['timos_oids'] = snmpwalk_cache_multi_oid($device, 'tmnxDDMTxOutputPowerHiAlarm', $pre_cache, 'TIMETRA-PORT-MIB', 'timos');
+$pre_cache['timos_oids'] = snmpwalk_cache_multi_oid($device, 'tmnxDDMTxOutputPowerHiWarning', $pre_cache, 'TIMETRA-PORT-MIB', 'timos');
